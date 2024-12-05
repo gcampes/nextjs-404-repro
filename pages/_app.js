ae4342949adc5b2a6430d5868a92e2d2d15677b3
Gabriel Carvalho de Campes <gdecampes@seatgeek.com>
2024-12-05 12:16:31 -0500

    Update next and re-add getInitialProps

--- pages/_app.js
+++ pages/_app.js
@@ -8,4 +8,10 @@ function _app({ Component, pageProps, err }) {
   );
 }
 
+_app.getInitialProps = async function getInitialProps(ctx) {
+  const appProps = await App.getInitialProps(ctx);
+
+  return appProps;
+};
+
 export default _app;
